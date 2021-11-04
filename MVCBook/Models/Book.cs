d81a562fed0d2af63726b420fa21574b4fc39ade
franarci <fran.arciniega96@gmail.com>
2021-11-04 19:43:20 -0300

    Armo clase Book

--- MVCBook/Models/Book.cs
+++ MVCBook/Models/Book.cs
@@ -7,5 +7,15 @@ namespace MVCBook.Models
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+        public int PagesNumber { get; set; }
+        public string Publisher { get; set; }
+        public string PublicationDate { get; set; }
+        public string Content { get; set; }
+        public decimal Price { get; set; }
+        public decimal PriceConfirm { get; set; }
     }
 }
