6c9f3694543797c642f4271d89723d883b0717ea
franarci <fran.arciniega96@gmail.com>
2021-11-04 20:17:45 -0300

    Annotations en Book

--- MVCBook/Models/Book.cs
+++ MVCBook/Models/Book.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MVCBook.Validations;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace MVCBook.Models
@@ -8,14 +11,25 @@ namespace MVCBook.Models
     public class Book
     {
         public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
-
+        [Required]
+        [StringLength(50)]
         public string Author { get; set; }
+        [Range(100,10000)]
         public int PagesNumber { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Publisher { get; set; }
+        [RegularExpression(@"^\d{4}(\-|V|\.)\d{1,2}$")]
         public string PublicationDate { get; set; }
-        public string Content { get; set; }
+        public string Content { get; set; }       
+        [DataType("money")]
         public decimal Price { get; set; }
+        [DataType("money")]
+        [PriceConfirmValidation("Price")]
         public decimal PriceConfirm { get; set; }
     }
 }
