23972e647e1bc9480e6f06f10b1fc3cb1dbf6d44
franarci <fran.arciniega96@gmail.com>
2021-11-04 20:17:23 -0300

    PriceConfirmValidation

---
+++ MVCBook/Validations/PriceConfirmValidation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCBook.Validations
+{
+    public class PriceConfirmValidation : CompareAttribute
+    {
+
+        public PriceConfirmValidation(string price) :base(price)
+        {
+            ErrorMessage = "'PriceConfirm' and 'Price' do not match.";
+        }
+        public override bool IsValid(object value)
+        {
+            decimal priceConfirm = Convert.ToDecimal(value);
+            return priceConfirm == Convert.ToDecimal(OtherProperty);
+        }
+    }
+}
