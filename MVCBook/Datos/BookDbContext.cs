2eb393cbddcc9a5d60f94525cfa2eeb080e23980
franarci <fran.arciniega96@gmail.com>
2021-11-04 22:40:54 -0300

    BookDbContext y BookInitializer

---
+++ MVCBook/Datos/BookDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MVCBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace MVCBook.Datos
+{
+    public class BookDbContext : DbContext
+    {
+       public DbSet<Book> Books { get; set; }
+
+       public BookDbContext() : base("KeyDB") { }
+    }
+}
---
+++ MVCBook/Datos/BookInitializer.cs
@@ -0,0 +1,32 @@
+﻿using MVCBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCBook.Datos
+{
+    public class BookInitializer : DropCreateDatabaseAlways<BookDbContext>
+    {
+
+        protected override void Seed(BookDbContext context)
+        {
+            var books = new List<Book>
+            {
+               new Book
+               {
+                  Name = "It",
+                  Author = "Stephen King",
+                  PagesNumber = 5000,
+                  Price = 250,
+                  PriceConfirm=250,
+                  Publisher ="Publisher",
+                  PublicationDate=new DateTime(1978,05,15)
+               }
+            };
+            books.ForEach(b => context.Books.Add(b));
+            context.SaveChanges();
+        }
+    }
+}
